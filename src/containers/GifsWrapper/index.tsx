04a5fa175a3b7d695e682133604da436efae1665
CamilaM <camila.miranda.moura@gmail.com>
2022-03-27 15:26:30 -0300

    added responsive style

--- src/containers/GifsWrapper/index.tsx
+++ src/containers/GifsWrapper/index.tsx
@@ -1,22 +1,28 @@
 import React, { useContext } from 'react'
 
+import useResponsive from '../../hooks/useResponsive'
+
 import { SearchBar, SearchContext } from '@giphy/react-components'
 import GifsGrid from '../../components/GifsGrid'
+
+import * as S from './styled'
 
 const GifsWrapper = () => {
   const { fetchGifs, term, channelSearch, activeChannel } =
     useContext(SearchContext)
 
+  const isMobile = useResponsive('(max-width: 48rem)')
+
   return (
-    <main>
+    <S.GifsContainer>
       <SearchBar />
       <GifsGrid
         key={`${channelSearch} ${term} ${activeChannel?.user.username}`}
-        columns={4}
-        width={600}
+        columns={isMobile ? 2 : 5}
+        width={isMobile ? 600 : 900}
         fetchGifs={fetchGifs}
       />
-    </main>
+    </S.GifsContainer>
   )
 }
 
